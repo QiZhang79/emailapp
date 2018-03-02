cc73be6af77a1cdc624d73738a8a56c855567117
Qi Zhang <zhangqitydx@gmail.com>
2018-03-01 23:43:47 -0600

    Initial Commit

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,12 @@
+const passport = require('passport'); // 'passport' refers to the npm module passport.js
+
+module.exports = app => {
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email']
+    })
+  );
+
+  app.get('/auth/google/callback', passport.authenticate('google'));
+};
